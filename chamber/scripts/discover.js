d69e2241f183289b03710b64a85e2816f489144f
didieravz <didiervz@hotmail.com>
2024-10-17 17:21:12 -0500

    Ajustes discover

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -1,12 +1,14 @@
 // Función para manejar el mensaje de visita
 function handleVisitMessage() {
     const visitMessage = document.getElementById("visit-message");
-    const lastVisit = localStorage.getItem("lastVisit");
+    const lastVisit = localStorage.getItem("ultimaVisita");
+    //contiene el tiempo actual en milisegundos
     const currentDate = new Date().getTime();
 
     if (!lastVisit) {
         visitMessage.textContent = "Welcome! Let us know if you have any questions.";
     } else {
+        //contiene el numero de dias transcurridos desde la ultima visita hasta la actualidad
         const daysSinceLastVisit = Math.floor((currentDate - parseInt(lastVisit)) / (1000 * 60 * 60 * 24));
 
         if (daysSinceLastVisit < 1) {
@@ -18,7 +20,7 @@ function handleVisitMessage() {
         }
     }
 
-    localStorage.setItem("lastVisit", currentDate.toString());
+    localStorage.setItem("ultimaVisita", currentDate.toString());
 }
 
 // Ejecutar cuando el DOM esté cargado
